2d407fc0a3a9e296c5487bf7e87b610464bf1500
mishobo <60912349+mishobo@users.noreply.github.com>
2022-08-22 11:46:46 +0300

    creating a custom Hook

--- src/App.js
+++ src/App.js
@@ -4,19 +4,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Dashboard from './components/Dashboard/Dashboard'
 import Preferences from './components/Preferences/Preferences'
 import Login from './components/Login/Login'
-
-function setToken(userToken) {
-  sessionStorage.setItem('token', JSON.stringify(userToken))
-}
-
-function getToken() {
-  const tokenString = sessionStorage.getItem('token')
-  const userToken = JSON.parse(tokenString)
-  return userToken?.token
-}
+import useToken from './components/App/useToken'
 
 function App() {
-  const token = getToken()
+  const { token, setToken } = useToken()
 
   if (!token) {
     return <Login setToken={setToken} />
