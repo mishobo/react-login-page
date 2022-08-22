232337f1502e13b2fe33995321ac46e4eab3eafb
mishobo <60912349+mishobo@users.noreply.github.com>
2022-08-22 09:08:42 +0300

    creating a Token API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React, { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Dashboard from './components/Dashboard/Dashboard'
 import Preferences from './components/Preferences/Preferences'
+import Login from './components/Login/Login'
 
 function App() {
+  const [token, setToken] = useState()
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
