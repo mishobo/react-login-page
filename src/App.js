4d6441932ea34ac4f9465c2bda5ed584a5de82b6
mishobo <60912349+mishobo@users.noreply.github.com>
2022-08-22 10:05:03 +0300

    session storage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Dashboard from './components/Dashboard/Dashboard'
 import Preferences from './components/Preferences/Preferences'
 import Login from './components/Login/Login'
 
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken))
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token')
+  const userToken = JSON.parse(tokenString)
+  return userToken?.token
+}
+
 function App() {
-  const [token, setToken] = useState()
+  const token = getToken()
 
   if (!token) {
     return <Login setToken={setToken} />
